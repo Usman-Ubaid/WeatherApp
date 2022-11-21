e68b7117f90dfa7cd813c7d8c48234a8f614d943
Usman Ubaid <usmanubaid98@outlook.com>
2022-11-21 12:25:52 +0100

    Styling the TimeAndLocation component.

--- weather-app/src/components/TimeAndLocation.js
+++ weather-app/src/components/TimeAndLocation.js
@@ -1,5 +1,17 @@
 const TimeAndLocation = () => {
-  return <div>TimeAndLocation</div>;
+  return (
+    <div>
+      <div className="text-center my-4">
+        <p className="text-white text-lg font-light">
+          Monday, 21 November 2022 | Local Time: 12:17 PM
+        </p>
+      </div>
+
+      <div className="text-center my-4">
+        <p className="text-white text-2xl font-bold">Berlin, DE</p>
+      </div>
+    </div>
+  );
 };
 
 export default TimeAndLocation;
