b9d222e39d69b83cf5f2780c91bb2ecfe6b85815
Usman Ubaid <usmanubaid98@outlook.com>
2022-11-21 10:56:10 +0100

    Added Top Buttons component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 h-fit bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-500">
+      <TopButtons />
     </div>
   );
 }
---
+++ weather-app/src/components/TopButtons.js
@@ -0,0 +1,35 @@
+function TopButtons() {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "Sydney",
+    },
+    {
+      id: 3,
+      title: "Toronto",
+    },
+    {
+      id: 4,
+      title: "Paris",
+    },
+    {
+      id: 5,
+      title: "Berlin",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center my-6">
+      {cities.map((city) => (
+        <button className="text-white text-lg font-medium hover:text-gray-200 ">
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
