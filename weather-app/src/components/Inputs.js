f45194f426c4f4f68c7ca02dacd40ba1d15d0d19
Usman Ubaid <usmanubaid98@outlook.com>
2022-11-25 22:41:47 +0100

    Added Navigator Web API to the component.

--- weather-app/src/components/Inputs.js
+++ weather-app/src/components/Inputs.js
@@ -1,27 +1,55 @@
 import { UilSearch, UilMapMarker } from "@iconscout/react-unicons";
+import { useState } from "react";
 
-const Inputs = () => {
+const Inputs = ({ setQuery, units, setUnits }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearchClick = () => {
+    if (city !== "") {
+      setQuery({ q: city });
+    }
+  };
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({ lat, lon });
+      });
+    }
+  };
+
   return (
     <div className="flex justify-between items-center my-6 ">
       <div className="flex justify-start items-center gap-x-3 w-3/4 ">
         <input
+          value={city}
           type="text"
           placeholder="Search a city..."
           className="outline-none p-1.5 text-base w-full capitalize placeholder:lowercase"
+          onChange={(e) => setCity(e.currentTarget.value)}
         />
         <UilSearch
           size={25}
           className="text-slate-100 cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilMapMarker
           size={25}
           className="text-slate-100 cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
         />
       </div>
       <div className="flex justify-end items-center gap-2 w-1/4 text-slate-100 font-normal text-lg">
-        <button name="metric"> °C </button>
+        <button name="metric" className="transition ease-out hover:scale-125">
+          °C
+        </button>
         <p>|</p>
-        <button name="imperial"> °F </button>
+        <button name="imperial" className="transition ease-out hover:scale-125">
+          °F
+        </button>
       </div>
     </div>
   );
