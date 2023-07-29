9a8061a98be21603d8774b7691579acd4108ee1b
Welcome ! <usmanubaid9@gmail.com>
2023-07-29 17:03:08 +0200

    refactor

--- weather-app/src/services/WeatherServices.js
+++ weather-app/src/services/WeatherServices.js
@@ -48,26 +48,6 @@ const formatCurrentWeather = (data) => {
   };
 };
 
-// const formatForecastWeather = (data) => {
-//   let { timezone, daily, hourly } = data;
-//   daily = daily.slice(1, 6).map((d) => {
-//     return {
-//       title: formatToLocalTime(d.dt, timezone, "ccc"),
-//       temp: d.temp.day,
-//       icon: d.weather[0].icon,
-//     };
-//   });
-//   hourly = hourly.slice(1, 6).map((d) => {
-//     return {
-//       title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
-//       temp: d.temp.day,
-//       icon: d.weather[0].icon,
-//     };
-//   });
-
-//   return { timezone, daily, hourly };
-// };
-
 const formattedWeatherData = async (searchParams) => {
   const formattedCurrentWeather = await getWeatherData(
     "weather",
