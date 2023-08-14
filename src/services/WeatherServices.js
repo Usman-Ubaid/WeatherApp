b11911ea2d4211537190875704f971b1b323c66d
Welcome ! <usmanubaid9@gmail.com>
2023-08-14 20:54:09 +0200

    refactor: add .env file

--- src/services/WeatherServices.js
+++ src/services/WeatherServices.js
@@ -1,15 +1,11 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "52f80901d7ba7feadd6526d8d3563262";
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
-
-// One Call API:  https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
 const getWeatherData = (infoType, searchParams) => {
-  // searchParams is an object. It could be longitude, latitude or city name.
-  // infoType in this case would be weather.
-  const url = new URL(BASE_URL + "/" + infoType);
-  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+  const url = new URL(process.env.REACT_APP_BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({
+    ...searchParams,
+    appid: `${process.env.REACT_APP_API_KEY}`,
+  });
 
   return fetch(url).then((res) => res.json());
 };
